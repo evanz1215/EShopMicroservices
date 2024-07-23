54f24420d30baff5173fd69cb0dc1484ba69fb06
Evan <evanz1215@gmail.com>
2024-07-24 00:24:15 +0800

    重構代碼以集中處理驗證
    - 在 CreateProductHandler.cs 中，移除了對 IValidator<CreateProductCommand> 的依賴注入，改為注入 ILogger<CreateProductCommandHandler>，並移除了原本用於驗證命令對象的代碼，添加了處理命令前的日誌記錄。
    - Program.cs 中添加了對 BuildingBlocks.Behaviors 的引用，改進了 MediatR 服務註冊的代碼結構，並將 ValidationBehavior<,> 加入到 MediatR 的配置中，以實現命令或查詢的統一驗證。同時，添加了對 Carter 的支持，以增強 API 路由和處理能力。
    - 新增了 ValidationBehavior.cs，實現了 MediatR 的 IPipelineBehavior 接口，用於在請求處理管道中進行統一驗證，並在發現驗證錯誤時拋出 ValidationException，這標誌著系統的驗證機制已從分散於各處理器轉移到集中的管道行為中。

--- src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -14,7 +14,7 @@ public class CreateProductCommandValidator : AbstractValidator<CreateProductComm
     }
 }
 
-internal class CreateProductCommandHandler(IDocumentSession session, IValidator<CreateProductCommand> validator) : ICommandHandler<CreateProductCommand, CreateProductResult>
+internal class CreateProductCommandHandler(IDocumentSession session, ILogger<CreateProductCommandHandler> logger /*IValidator<CreateProductCommand> validator*/) : ICommandHandler<CreateProductCommand, CreateProductResult>
 {
     public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
     {
@@ -22,13 +22,15 @@ internal class CreateProductCommandHandler(IDocumentSession session, IValidator<
         // save to database
         // return CreateProductResult result
 
-        var result = await validator.ValidateAsync(command, cancellationToken);
-        var errors = result.Errors.Select(x => x.ErrorMessage).ToList();
+        //var result = await validator.ValidateAsync(command, cancellationToken);
+        //var errors = result.Errors.Select(x => x.ErrorMessage).ToList();
 
-        if (result.Errors.Any())
-        {
-            throw new ValidationException(errors.FirstOrDefault());
-        }
+        //if (result.Errors.Any())
+        //{
+        //    throw new ValidationException(errors.FirstOrDefault());
+        //}
+
+        logger.LogInformation("CreateProductCommandHandler.Handle called with {@Command}", command);
 
         var product = new Product
         {
