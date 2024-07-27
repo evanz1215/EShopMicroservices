35ad6ae95bfd1250203d5cadacfbb7f83c831a0e
Evan <evanz1215@gmail.com>
2024-07-28 02:24:58 +0800

    Basket操作

--- src/Services/Basket/Basket.API/Basket/StoreBasket/StoreBasketHandler.cs
+++ src/Services/Basket/Basket.API/Basket/StoreBasket/StoreBasketHandler.cs
@@ -13,15 +13,13 @@ public class StoreBasketCommandValidator : AbstractValidator<StoreBasketCommand>
     }
 }
 
-public class StoreBasketCommandHandler : ICommandHandler<StoreBasketCommand, StoreBasketResult>
+public class StoreBasketCommandHandler(IBasketRepository repository)
+    : ICommandHandler<StoreBasketCommand, StoreBasketResult>
 {
     public async Task<StoreBasketResult> Handle(StoreBasketCommand command, CancellationToken cancellationToken)
     {
-        ShoppingCart cart = command.Cart;
+        var basket = await repository.StoreBasket(command.Cart, cancellationToken);
 
-        //TODO: store basket in database (use Marten ipsert - if exists update, if not insert)
-        //TODO: update cache
-
-        return new StoreBasketResult("swn");
+        return new StoreBasketResult(command.Cart.UserName);
     }
 }
