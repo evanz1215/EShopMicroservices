11b43e15be79f97d27989af6b2b82e0b04ee909a
Evan <evanz1215@gmail.com>
2024-07-29 15:54:35 +0800

    新增Discount gRPC專案

--- src/Services/Basket/Basket.API/Program.cs
+++ src/Services/Basket/Basket.API/Program.cs
@@ -1,4 +1,6 @@
-using Weasel.Core;
+using HealthChecks.UI.Client;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+using Microsoft.Extensions.Caching.Distributed;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -14,12 +16,36 @@ builder.Services.AddMediatR(config =>
 builder.Services.AddMarten(opts =>
     {
         opts.Connection(builder.Configuration.GetConnectionString("Database")!);
-        opts.AutoCreateSchemaObjects = AutoCreate.CreateOrUpdate;
+        // opts.AutoCreateSchemaObjects = AutoCreate.CreateOrUpdate;
         opts.Schema.For<ShoppingCart>().Identity(x => x.UserName);
     })
     .UseIdentitySessions();
 
 builder.Services.AddScoped<IBasketRepository, BasketRepository>();
+
+// NOTE: 2.使用Scrutor套件
+builder.Services.Decorate<IBasketRepository, CachedBasketRepository>();
+
+// builder.Services.AddScoped<IBasketRepository, CachedBasketRepository>();
+
+// NOTE: 1.這種做法麻煩請不易擴充
+// builder.Services.AddScoped<IBasketRepository>(provider =>
+// {
+//     var basketRepository = provider.GetRequiredService<BasketRepository>();
+//     return new CachedBasketRepository(basketRepository, provider.GetRequiredService<IDistributedCache>());
+// });
+
+builder.Services.AddStackExchangeRedisCache(options =>
+{
+    options.Configuration = builder.Configuration.GetConnectionString("Redis");
+    // options.InstanceName = "Basket";
+});
+
+builder.Services.AddExceptionHandler<CustomExceptionHandler>();
+
+builder.Services.AddHealthChecks()
+    .AddNpgSql(builder.Configuration.GetConnectionString("Database")!)
+    .AddRedis(builder.Configuration.GetConnectionString("Redis")!);
 
 // add sevices to the container.
 var app = builder.Build();
@@ -27,4 +53,11 @@ var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 app.MapCarter();
+app.UseExceptionHandler(options => { });
+
+app.UseHealthChecks("/health", new HealthCheckOptions
+{
+    ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+});
+
 app.Run();
