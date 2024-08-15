0d1120b7672c5be9ad9d9e93c013b6412c220647
Evan <evanz1215@gmail.com>
2024-08-15 23:12:37 +0800

    更新 Dockerfile 和配置文件，優化服務構建和連接
    - 將 Dockerfile 中的註解從英文改為中文，並新增多個階段來構建和發佈服務專案。
    - 在 `appsettings.json` 中，將 `OrderFullfilment` 功能管理的值從 `true` 改為 `false`。
    - 在 `GetOrdersQuery.cs` 中，將 `GetOrdersResult` 記錄中的屬性名稱從 `orders` 改為 `Orders`。
    - 在 `OrderName.cs` 中，註解掉了 `ArgumentOutOfRangeException.ThrowIfNotEqual` 檢查。
    - 在 `docker-compose.override.yml` 中，為 `services` 新增了 `MessageBroker` 的連接設定，並新增了 `messagebroker` 服務的依賴。
    - 在 `docker-compose.override.yml` 中，為 `ordering.api` 新增了資料庫和 `MessageBroker` 的連接設定，並新增了 `orderdb` 和 `messagebroker` 服務的依賴。
    - 新增了一個名為 `Dockerfile1.original` 的 Dockerfile，包含多個階段來構建和發佈服務專案。

--- src/Services/Ordering/Ordering.Application/Orders/Queries/GetOrders/GetOrdersQuery.cs
+++ src/Services/Ordering/Ordering.Application/Orders/Queries/GetOrders/GetOrdersQuery.cs
@@ -3,4 +3,4 @@
 namespace Ordering.Application.Orders.Queries.GetOrders;
 
 public record GetOrdersQuery(PaginatedRequest PaginatedRequest) : IQuery<GetOrdersResult>;
-public record GetOrdersResult(PaginatedResult<OrderDto> orders);
+public record GetOrdersResult(PaginatedResult<OrderDto> Orders);
